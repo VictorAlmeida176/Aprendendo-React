c09c2ba158784788a79e42bf9e96104e83d4d3c0
victoralmeida1766 <victoralmeida1766@gmail.com>
2021-09-27 13:05:45 -0300

    Criando um componente em React

---
+++ src/components/Repository_list.jsx
@@ -0,0 +1,46 @@
+const repositoryName = "VICTOR"
+
+export function Repository_list(){
+    return(
+    <section className="repository_list">
+        <h1>Lista de repositórios</h1>
+
+        <ul>
+            <li>
+                <strong>{repositoryName}</strong>
+                <p>Forms in React</p>
+
+                <a>Acessar repositório</a>
+            </li>
+        </ul>
+
+        <ul>
+            <li>
+                <strong>Unform</strong>
+                <p>Forms in React</p>
+
+                <a>Acessar repositório</a>
+            </li>
+        </ul>
+
+        <ul>
+            <li>
+                <strong>Unform</strong>
+                <p>Forms in React</p>
+
+                <a>Acessar repositório</a>
+            </li>
+        </ul>
+
+        <ul>
+            <li>
+                <strong>Unform</strong>
+                <p>Forms in React</p>
+
+                <a>Acessar repositório</a>
+            </li>
+        </ul>
+
+    </section>
+    );
+}
